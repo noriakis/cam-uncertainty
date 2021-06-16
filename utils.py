b605c0244d7f6f8d4288e4f59ec8fe901077686d
noriakis <noriaki.sato53@gmail.com>
2021-06-16 11:44:43 +0900

    1.0.3

--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
 import cv2
-import keras
 import numpy as np
 from PIL import Image
 from tqdm import tqdm
@@ -50,6 +49,48 @@ def GradCam(input_model, image, category_index, layer_name, raw_array, dimension
     return np.uint8(cam), heatmap
 
 
+def ScoreCam(input_model, image, category_index, layer_name, raw_array, dimension):
+    # Implementation reference
+    # https://github.com/tabayashi0117/Score-CAM
+    # https://github.com/haofanwang/Score-CAM
+    input_shape = (dimension, dimension)
+    act_map_array = tf.keras.Model(inputs=input_model.input, outputs=input_model.get_layer(layer_name).output)(image).numpy()
+    act_map_resized_list = [cv2.resize(act_map_array[0,:,:,k], input_shape, interpolation=cv2.INTER_LINEAR) for k in range(act_map_array.shape[3])]
+    act_map_normalized_list = []
+    for act_map_resized in act_map_resized_list:
+        if np.max(act_map_resized) - np.min(act_map_resized) != 0:
+            act_map_normalized = act_map_resized / (np.max(act_map_resized) - np.min(act_map_resized))
+        else:
+            act_map_normalized = act_map_resized
+        act_map_normalized_list.append(act_map_normalized)
+    masked_input_list = []
+    for act_map_normalized in act_map_normalized_list:
+        masked_input = np.copy(image)
+        for k in range(3):
+            masked_input[0,:,:,k] *= act_map_normalized
+        masked_input_list.append(masked_input)
+    masked_input_array = np.concatenate(masked_input_list, axis=0)
+
+    # No memory available
+    input_split = np.array_split(masked_input_array, 40)
+    split_weights = []
+    for batch in input_split:
+        w = input_model(batch)
+        split_weights.append(w)
+    pred_from_masked_input_array = np.vstack(split_weights)
+    weights = pred_from_masked_input_array[:, category_index]
+    cam = np.dot(act_map_array[0,:,:,:], weights)
+    cam = cv2.resize(cam, (dimension, dimension))
+    score_heatmap = cam.copy()
+    cam = np.maximum(0, cam)  # Passing through ReLU
+    cam /= np.max(cam)  # scale 0 to 1.0
+
+    raw_image = np.asarray(Image.fromarray(raw_array.astype("uint8")).resize((dimension, dimension)))
+    cam = cv2.applyColorMap(np.uint8(255-255*cam), cv2.COLORMAP_JET)
+    cam = cv2.addWeighted(cam, 0.5, raw_image, 0.5, 0)
+
+    return np.uint8(cam), score_heatmap
+
 def GradCam_Dropout(input_model, image, category_index, layer_name, raw_array, dimension, sample):
     gradModel = tf.keras.Model(
             inputs=[input_model.inputs],
@@ -88,3 +129,56 @@ def GradCam_Dropout(input_model, image, category_index, layer_name, raw_array, d
     w_cam = cv2.addWeighted(w_cam, 0.5, image, 0.5, 0)
 
     return np.uint8(w_cam), heatmap
+
+
+def ScoreCam_Dropout(input_model, image, category_index, layer_name, raw_array, dimension, sample):
+    input_shape = (dimension, dimension)
+    cams = []
+    for i in tqdm(range(0, sample)):
+        act_map_array = tf.keras.Model(inputs=input_model.input, outputs=input_model.get_layer(layer_name).output)(image).numpy()
+        act_map_resized_list = [cv2.resize(act_map_array[0,:,:,k], input_shape, interpolation=cv2.INTER_LINEAR) for k in range(act_map_array.shape[3])]
+        act_map_normalized_list = []
+        for act_map_resized in act_map_resized_list:
+            if np.max(act_map_resized) - np.min(act_map_resized) != 0:
+                act_map_normalized = act_map_resized / (np.max(act_map_resized) - np.min(act_map_resized))
+            else:
+                act_map_normalized = act_map_resized
+            act_map_normalized_list.append(act_map_normalized)
+        masked_input_list = []
+        for act_map_normalized in act_map_normalized_list:
+            masked_input = np.copy(image)
+            for k in range(3):
+                masked_input[0,:,:,k] *= act_map_normalized
+            masked_input_list.append(masked_input)
+        masked_input_array = np.concatenate(masked_input_list, axis=0)
+        
+        # No memory available
+        input_split = np.array_split(masked_input_array, 40)
+
+        split_weights = []
+        for batch in input_split:
+            w = input_model(batch)
+            split_weights.append(w)
+        pred_from_masked_input_array = np.vstack(split_weights)
+        weights = pred_from_masked_input_array[:, category_index]
+        cam = np.dot(act_map_array[0,:,:,:], weights)
+        cam = cv2.resize(cam, (dimension, dimension))
+        cam = np.maximum(0, cam) 
+        cam /= np.max(cam)
+        cams.append(cam)
+
+    m = np.mean(cams, axis=0)
+    std = np.std(cams, axis=0)
+    cov = std / m
+
+    newcam = m  * (1-cov)
+    newcam = cv2.resize(newcam, (dimension, dimension))
+    score_heatmap = newcam.copy()
+    newcam = np.maximum(0, newcam)
+    newcam /= np.max(newcam)
+
+    raw_image = np.asarray(Image.fromarray(raw_array.astype("uint8")).resize((dimension, dimension)))
+    newcam = cv2.applyColorMap(np.uint8(255-255*newcam), cv2.COLORMAP_JET)
+    newcam = cv2.addWeighted(newcam, 0.5, raw_image, 0.5, 0)
+
+    return np.uint8(newcam), score_heatmap
